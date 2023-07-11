2be993dabfcfffbf02b00f00cbef7a5ec4b0c9e1
Daniel Chukwudubem <princedaniel7890@gmail.com>
2023-07-11 13:47:24 +0100

    added more logic to the reducer function for some other actions

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,21 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "add-todo":
       return {
-        todos: [...state.todos, { text: action.text, completed: false }],
+        todos: [
+          ...state.todos,
+          { text: action.text, completed: false, id: Math.random() },
+        ],
+        todosCount: state.todosCount + 1,
       };
+    case "toggle-todo":
+      return {
+        todos: state.todos.map((t, i) => {
+          i === action.i ? { ...t, completed: !t.completed } : t;
+        }),
+
+        // todosCount: state.todosCount,
+      };
+      console.log(completed);
 
     default:
       return state;
@@ -14,8 +27,11 @@ const reducer = (state, action) => {
 };
 
 function App() {
-  const [{ todos }, dispatch] = useReducer(reducer, { todos: [] });
-  const [text, setText] = useState();
+  const [{ todos, todosCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todosCount: 0,
+  });
+  const [text, setText] = useState("");
 
   const inputHandler = (e) => {
     const name = e.target.value;
@@ -28,12 +44,28 @@ function App() {
     setText("");
   };
 
+  // const onClickHandler = () => {
+  //   dispatch({type: "toggle-todo", i})
+  // }
+
   return (
     <>
       <form onSubmit={submitHandler}>
         <input type="text" value={text} onChange={inputHandler} />
       </form>
-      <div>{JSON.stringify(todos, null, 2)}</div>
+      <p>you have {todosCount} todos</p>
+      <div>
+        {todos.map((t, i) => (
+          <div
+            key={t.id}
+            onClick={() => dispatch({ type: "toggle-todo", i })}
+            // style={{ textDecoration: t.completed ? "line-through" : "" }}
+          >
+            {t.text}
+          </div>
+        ))}
+      </div>
+      {/* <div>{JSON.stringify(todos)}</div> */}
     </>
   );
 }
